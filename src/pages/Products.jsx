1c0587ce8599593b1211ed5c5034eebf3ba89bc6
solovyovsdom <joelborofsky@mac.com>
2023-09-17 01:22:52 -0500

    added dynamic links

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,25 @@
+import { Link } from 'react-router-dom'
+
+const PRODUCTS = [
+  { id: 'p1', title: 'Gaming Mouse', price: 29.99 },
+  { id: 'p2', title: 'Harry Potter 3', price: 9.99 },
+  { id: 'p3', title: 'Used plastic bottle', price: 0.99 },
+  { id: 'p4', title: 'Half-dried plant', price: 2.99 },
+]
+
 function Products() {
   return (
     <>
       <h2>The products page</h2>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        <li>Product 4</li>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              {' '}
+              {product.title} - ${product.price}
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   )
