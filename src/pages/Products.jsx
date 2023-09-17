fbe00fe82980cec2ce5b8a3b9fb1a916175d4efa
solovyovsdom <joelborofsky@mac.com>
2023-09-17 01:16:36 -0500

    added dynamic paths

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,14 @@
 function Products() {
   return (
-    <h2>The products page</h2>
+    <>
+      <h2>The products page</h2>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+      </ul>
+    </>
   )
 }
 export default Products
